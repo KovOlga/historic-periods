e56e60ac1f96d139d3959b01e3e2758cf6b1d35b
KovOlga <switch.to.olga@yandex.ru>
2025-05-16 23:37:34 +0300

    arrow icon styles

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -1,8 +1,9 @@
 import { ReactElement } from 'react';
+import styles from './styles.module.scss';
 
 export function LeftArrowIcon({ disabled, color }: { disabled?: boolean; color: string }): ReactElement {
   return (
-    <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg className={styles.arrow} viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M8.49988 0.750001L2.24988 7L8.49988 13.25" stroke={disabled ? '#42567a80' : color} strokeWidth="2" />
     </svg>
   );
@@ -10,7 +11,7 @@ export function LeftArrowIcon({ disabled, color }: { disabled?: boolean; color: 
 
 export function RightArrowIcon({ disabled, color }: { disabled?: boolean; color: string }): ReactElement {
   return (
-    <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg className={styles.arrow} viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M1.50012 0.750001L7.75012 7L1.50012 13.25" stroke={disabled ? '#42567a80' : color} strokeWidth="2" />
     </svg>
   );
